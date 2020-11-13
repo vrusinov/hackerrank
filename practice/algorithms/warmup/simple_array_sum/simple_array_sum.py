836001ce4bbc81f04799b24b6e7c30d19a48603f
Vladimir Rusinov <vladimir.rusinov@gmail.com>
2020-11-13 11:05:46 +0000

    Simple Array Sum solved

---
+++ practice/algorithms/warmup/simple_array_sum/simple_array_sum.py
@@ -0,0 +1,24 @@
+#!/bin/python3
+"""Simple Array Sum
+
+https://www.hackerrank.com/challenges/simple-array-sum/problem
+"""
+
+import os
+import sys
+
+def simpleArraySum(ar):
+    return sum(ar)
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    ar_count = int(input())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = simpleArraySum(ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
