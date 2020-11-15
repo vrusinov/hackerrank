ead7d1c13da4284941852baed30082c378f6194f
Vladimir Rusinov <vladimir.rusinov@gmail.com>
2020-11-15 18:10:16 +0000

    Sales by Match.

---
+++ practice/interview_preparation_kit/warm_up_challenges/sales_by_match/sales_by_match.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""Sales by Match.
+
+https://www.hackerrank.com/challenges/sock-merchant/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=warmup
+"""
+
+import math
+import os
+import random
+import re
+import sys
+import collections
+
+
+def sockMerchant(n, ar):
+    socks_per_color = collections.Counter()
+    for color in ar:
+        socks_per_color[color] += 1
+    total_paris = 0
+    for color, num in socks_per_color.items():
+        total_paris += num // 2
+    return total_paris
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = sockMerchant(n, ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
