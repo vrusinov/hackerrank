5672569d7d0b01f63999d2b37e808e1d12642c72
Vladimir Rusinov <vladimir.rusinov@gmail.com>
2022-03-27 20:30:21 +0100

    Left rotation - Python

---
+++ practice/interview_preparation_kit/arrays/left_rotation/left_rotation.py
@@ -0,0 +1,48 @@
+#!/bin/python3
+"""
+Hackerrank Left Rotation problem
+
+https://www.hackerrank.com/challenges/ctci-array-left-rotation/problem
+"""
+
+import os
+
+def rotLeft(a, d):
+    """
+    Performs left rotation of array a by d rotations.
+
+    Args:
+        a: input array.
+        d: number of rotations to do.
+    Returns:
+        list of ints, rotated array.
+    """
+    if d == len(a):
+        return a
+    # No point in rotating over len of array.
+    if d > len(a):
+        d = d % len(a)
+    # Very easy to do in python - split array by 2 in d'th
+    # emelent. Join two of them together.
+    return a[d:] + a[:d]
+
+
+if __name__ == '__main__':
+    output_path = os.environ.get('OUTPUT_PATH', '/dev/stdout')
+    fptr = open(output_path, 'w')
+
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    d = int(first_multiple_input[1])
+
+    a = list(map(int, input().rstrip().split()))
+
+    result = rotLeft(a, d)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
---
+++ practice/interview_preparation_kit/arrays/left_rotation/left_rotation_test.py
@@ -0,0 +1,25 @@
+import unittest
+import left_rotation
+
+from parameterized import parameterized
+
+
+class LeftRotationTest(unittest.TestCase):
+
+    @parameterized.expand([
+        ([1, 2, 3, 4, 5], 4, [5, 1, 2, 3, 4]),
+        ([1, 2, 3, 4, 5], 5, [1, 2, 3, 4, 5]),
+        ([1, 2, 3, 4, 5], 9, [5, 1, 2, 3, 4]),
+        ([1, 2, 3, 4, 5], 0, [1, 2, 3, 4, 5]),
+    ])
+    def test_happy(self, a, d, expected):
+        self.assertListEqual(left_rotation.rotLeft(a, d), expected)
+
+
+    def test_boolean(self):
+        a = True
+        b = True
+        self.assertEqual(a, b)
+
+if __name__ == '__main__':
+    unittest.main()
