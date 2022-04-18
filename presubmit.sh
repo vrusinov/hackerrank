a3832b85c7da82ea71aec196b61df2b50fb57ade
Vladimir Rusinov <vladimir.rusinov@gmail.com>
2022-04-18 14:17:42 +0100

    Use golint

--- presubmit.sh
+++ presubmit.sh
@@ -6,7 +6,6 @@ docker run --rm -e "WORKSPACE=${PWD}" -v "$PWD:/app" shiftleft/sast-scan scan --
 
 # Run gometalinter in all go directories
 for d in $(find . -name \*.go | sed -r 's|/[^/]+$||' |sort -u) ; do
-    pushd $d > /dev/null
-    gometalinter
-    popd
+    golint $d
+    gometalinter $d
 done
