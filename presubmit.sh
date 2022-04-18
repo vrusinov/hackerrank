a886fbda81eb220fe11c4835076beecda528c590
Vladimir Rusinov <vladimir.rusinov@gmail.com>
2022-04-18 11:30:19 +0100

    gometalinter presubmit

--- presubmit.sh
+++ presubmit.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
+set -e
+
 docker run --rm -e "WORKSPACE=${PWD}" -v "$PWD:/app" shiftleft/sast-scan scan --build
+
+# Run gometalinter in all go directories
+for d in $(find . -name \*.go | sed -r 's|/[^/]+$||' |sort -u) ; do
+    pushd $d > /dev/null
+    gometalinter
+    popd
+done
