7353932ae714633e1963d55400daa67bc86bd31b
Vladimir Rusinov <vladimir.rusinov@gmail.com>
2022-12-16 14:18:03 +0000

    Add type annotations

--- practice/algorithms/warmup/simple_array_sum/simple_array_sum.py
+++ practice/algorithms/warmup/simple_array_sum/simple_array_sum.py
@@ -1,7 +1,7 @@
 #!/bin/python3
 
 # SPDX-FileCopyrightText: 2022 Vladimir Rusinov
-#
+# SPDX-FileCopyrightText: 2022 Google Inc
 # SPDX-License-Identifier: Apache-2.0
 
 """Simple Array Sum
@@ -14,18 +14,15 @@ import sys
 
 from typing import Sequence
 
-def simpleArraySum(ar: Sequence):
+def simpleArraySum(ar: Sequence[int]) -> int:
     return sum(ar)
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
-
     ar_count = int(input())
-
     ar = list(map(int, input().rstrip().split()))
 
     result = simpleArraySum(ar)
 
     fptr.write(str(result) + '\n')
-
     fptr.close()
