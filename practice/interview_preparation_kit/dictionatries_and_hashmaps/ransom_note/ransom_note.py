e02b6c5cd8c74688e33e9866c22ee8581e0e5b82
Vladimir Rusinov <vladimir.rusinov@gmail.com>
2022-12-25 18:00:56 +0000

    Ransom note

---
+++ practice/interview_preparation_kit/dictionatries_and_hashmaps/ransom_note/ransom_note.py
@@ -0,0 +1,33 @@
+#!/bin/python3
+# SPDX-FileCopyrightText: 2022 Vladimir Rusinov
+#
+# SPDX-License-Identifier: Apache-2.0
+"""Ransom Note - https://www.hackerrank.com/challenges/ctci-ransom-note/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=dictionaries-hashmaps
+"""
+
+import collections
+
+def check_magazine(magazine, note):
+    magazine_words = collections.Counter()
+    for word in magazine.split():
+        magazine_words[word] += 1
+    for word in note.split():
+        if magazine_words[word]:
+            magazine_words[word] -= 1
+        else:
+            return False
+    return True
+
+if __name__ == '__main__':
+    first_multiple_input = input().rstrip().split()
+
+    unused_m = int(first_multiple_input[0])
+    unused_n = int(first_multiple_input[1])
+
+    magazine = input().rstrip()
+    note = input().rstrip()
+
+    if check_magazine(magazine, note):
+        print("Yes")
+    else:
+        print("No")
---
+++ practice/interview_preparation_kit/dictionatries_and_hashmaps/ransom_note/ransom_note_test.py
@@ -0,0 +1,23 @@
+# SPDX-FileCopyrightText: 2022 Vladimir Rusinov
+#
+# SPDX-License-Identifier: Apache-2.0
+
+import unittest
+import ransom_note
+
+from parameterized import parameterized
+
+
+class RansomNoteTest(unittest.TestCase):
+
+    @parameterized.expand([
+        ("give me one grand today night", "give one grand today", True),
+        ("two times three is not four", "two times two is four", False),
+        ("ive got a lovely bunch of coconuts", "ive got some coconuts", False),
+    ])
+    def test_happy(self, magazine, note, expected):
+        self.assertEqual(ransom_note.check_magazine(magazine, note), expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
