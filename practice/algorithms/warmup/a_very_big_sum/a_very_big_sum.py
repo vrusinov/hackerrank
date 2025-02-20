6eb273762a1e8baa949f619d15a318241ad01667
Vladimir Rusinov <vladimir.rusinov@gmail.com>
2025-02-20 22:51:31 +0000

    Update pre-commit

--- practice/algorithms/warmup/a_very_big_sum/a_very_big_sum.py
+++ practice/algorithms/warmup/a_very_big_sum/a_very_big_sum.py
@@ -1,30 +1,32 @@
 #!/bin/python3
 
-# SPDX-FileCopyrightText: 2022 Vladimir Rusinov
-#
+# SPDX-FileCopyrightText: 2022,2025 Vladimir Rusinov
 # SPDX-License-Identifier: Apache-2.0
 
-"""A Very Big Sum
+"""A Very Big Sum.
 
 https://www.hackerrank.com/challenges/a-very-big-sum/problem
 """
 
 import os
+from typing import Iterable
 
 
-def aVeryBigSum(ar):
-    return sum(ar)
+def a_very_big_sum(arr: Iterable[int]) -> int:
+    """Calculate a very big sum.
+
+    Args:
+        arr: an arary to sum.
+
+    Returns:
+        a sum of all elements of array.
+    """
+    return sum(arr)
 
 
 if __name__ == '__main__':
-    fptr = open(os.environ['OUTPUT_PATH'], 'w')
-
-    ar_count = int(input())
-
-    ar = list(map(int, input().rstrip().split()))
-
-    result = aVeryBigSum(ar)
-
-    fptr.write(str(result) + '\n')
-
-    fptr.close()
+    with open(os.environ['OUTPUT_PATH'], 'w', encoding='utf-8') as fptr:
+        ar_count = int(input())
+        ar = list(map(int, input().rstrip().split()))
+        result = a_very_big_sum(ar)
+        fptr.write(str(result) + '\n')
